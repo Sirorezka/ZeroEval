c395853c452f6753503e5352430ce0c120f91c56
Ivan Petrov <sirorezka@gmail.com>
2026-03-13 00:59:14 +0300

    Updated readme

--- zero_score_gens.sh
+++ zero_score_gens.sh
@@ -14,18 +14,36 @@ while getopts ":d:" opt; do
 done
 
 
-if [ $DATA_NAME == "zebra_grid" ]; then
-    # result_dirs/zebra-grid.summary.md
+if [ $DATA_NAME == "all" ]; then
+    python -m src.evaluation.summarize  
+
+elif [ $DATA_NAME == "zebra_grid" ]; then
     python -m src.evaluation.zebra_grid_eval    
+
 elif [ $DATA_NAME == "hendrycks-math" ]; then
-    # result_dirs/zebra-grid.summary.md
     python -m src.evaluation.hendrycks_math_eval
+
 elif [ $DATA_NAME == "mmlu-pro" ]; then
-    # result_dirs/zebra-grid.summary.md
     python -m src.evaluation.mmlu_pro_eval mmlu-pro
+
 elif [ $DATA_NAME == "mmlu-pro-short" ]; then
-    # result_dirs/zebra-grid.summary.md
     python -m src.evaluation.mmlu_pro_eval mmlu-pro-short
+
+elif [ $DATA_NAME == "crux" ]; then
+    python -m src.evaluation.crux_eval
+
+elif [ $DATA_NAME == "math-l5" ]; then
+    python -m src.evaluation.math_eval math-l5
+
+elif [ $DATA_NAME == "gsm" ]; then
+    python -m src.evaluation.math_eval gsm
+
+elif [ $DATA_NAME == "mmlu-redux" ]; then
+    python -m src.evaluation.mcqa_eval mmlu-redux
+
+elif [ $DATA_NAME == "gplanet" ]; then
+    python -m src.evaluation.gplanet_eval
+
 else
     echo "Can't get accuracy scores for dataset. Invalid dataset_name: '$DATA_NAME'"
     exit 1
